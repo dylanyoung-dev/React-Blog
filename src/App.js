afff5a693e143558088de3017edfb7a78ae3ec7b
sitecoremaster <5227854+sitecoremaster@users.noreply.github.com>
2019-04-13 16:15:43 -0700

    Changes using Json Server with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    let url = "http://localhost:3001/posts"
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let posts = data.map((post, index) => {
+          return (
+            <div key={index}>
+              <h3>{post.title}</h3>
+              <p>Tags: {post.tags}</p>
+            </div>
+          )
+        })
+        this.setState({posts: posts});
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.posts}
       </div>
     );
   }
